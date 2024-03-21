edfcdf070377e920768bfde79f1ace64ac23d686
lexy <olakunle.irantiola@gmail.com>
2024-03-21 23:28:10 +0100

    updated first page

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {
+      app_name: "Avado Finance",
+      version: 1.0
+    };
   }
 }
